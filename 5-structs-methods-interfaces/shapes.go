86a1deba82d3be77d43fbe02ddd87f5ba792de91
Tom Koch <tomkoch@mailbox.org>
2021-12-19 13:40:16 +0100

    Add perimeter tests

--- 5-structs-methods-interfaces/shapes.go
+++ 5-structs-methods-interfaces/shapes.go
@@ -11,25 +11,29 @@ type Rectangle struct {
 	Height float64
 }
 
-type Circle struct {
-	Radius float64
-}
-
-type Triangle struct {
-	Base   float64
-	Height float64
-}
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Height + rectangle.Width)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
 }
 
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func (t Triangle) Area() float64 {
--- 5-structs-methods-interfaces/shapes_test.go
+++ 5-structs-methods-interfaces/shapes_test.go
@@ -3,13 +3,25 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
+		want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		want := 62.831853071795865
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
 func TestArea(t *testing.T) {
 	areaTests := []struct {
