74258827668cd99e2e2aad827c4e9ce245579f19
Tom Koch <tomkoch@mailbox.org>
2021-12-19 13:17:06 +0100

    Refactor TestArea

--- 5-structs-methods-interfaces/shapes_test.go
+++ 5-structs-methods-interfaces/shapes_test.go
@@ -13,20 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Height: 6, Width: 12}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Height: 6, Width: 12}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-        // using tt.name from the case to use it as the `t.Run` test name
-        t.Run(tt.name, func(t *testing.T) {
-            got := tt.shape.Area()
-            if got != tt.hasArea {
-                t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
-            }
-        })
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
