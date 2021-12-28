53ec164be0f024b1d7e47cb7cb379023ac1a6e1b
Tom Koch <tomkoch@mailbox.org>
2021-12-28 13:39:00 +0100

    Bitcoin wallet test

---
+++ 6-pointers-errors/wallet_test.go
@@ -0,0 +1,57 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		err := wallet.Withdraw(Bitcoin(5))
+
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(5))
+	})
+
+	t.Run("Withdraw with insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, ErrInsufficientFunds)
+	})
+
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
