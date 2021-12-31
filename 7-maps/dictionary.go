e5b8a1ca14012e7321756f5eb74d931179cdf7d2
Tom Koch <tomkoch@mailbox.org>
2021-12-31 14:14:16 +0100

    Add dictionary test & implementation

---
+++ 7-maps/dictionary.go
@@ -0,0 +1,56 @@
+package dictionary
+
+type Dictionary map[string]string
+
+const (
+	ErrWordExists       = DictionaryErr("word already exists in dictionary")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordDoesNotExist = DictionaryErr("the word you want to update does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
