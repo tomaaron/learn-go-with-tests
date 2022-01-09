45319b07c932267b6fb94dbbd948a22e7da22cad
Tom Koch <tomkoch@mailbox.org>
2022-01-09 14:20:26 +0100

    Refactor mocking

--- 9-mocking/main.go
+++ 9-mocking/main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWord = "Go!"
+const countdownStart = 3
 
 // Countdown prints a countdown from 3 to out.
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
