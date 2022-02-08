04ecf325d16b72483b2d0608e8cd4fb19bf27fce
Tom Koch <tomkoch@mailbox.org>
2022-02-08 20:24:09 +0100

    [14-context] Refactor

--- 14-context/context_test.go
+++ 14-context/context_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -12,6 +13,24 @@ import (
 type SpyStore struct {
 	response string
 	t        *testing.T
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
 
 func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
@@ -56,4 +75,22 @@ func TestServer(t *testing.T) {
 		}
 	})
 
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := &SpyResponseWriter{}
+
+		svr.ServeHTTP(response, request)
+
+		if response.written {
+			t.Errorf("a response should not have been written")
+		}
+	})
 }
