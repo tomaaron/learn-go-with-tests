0ec538d444852cfa04f25f93a01c5c4bc696684b
Tom Koch <tomkoch@mailbox.org>
2022-02-19 20:28:36 +0100

    Extend numeral convertoer till X

--- 15-property/numeral.go
+++ 15-property/numeral.go
@@ -1,11 +1,32 @@
 package numeral
 
+import (
+	"strings"
+)
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 3 {
-		return "III"
+
+	var result strings.Builder
+
+	for arabic > 0 {
+		switch {
+		case arabic > 9:
+			result.WriteString("X")
+			arabic -= 10
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
-	if arabic == 2 {
-		return "II"
-	}
-	return "I"
+
+	return result.String()
 }
--- 15-property/numeral_test.go
+++ 15-property/numeral_test.go
@@ -11,6 +11,11 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to V", 6, "VI"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
 	}
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
