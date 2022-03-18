4c6955b20a589047d65d942838be29030d73ecd6
Tom Koch <tomkoch@mailbox.org>
2022-03-18 10:49:20 +0100

    Refactor test

--- 16-maths/clockface_test.go
+++ 16-maths/clockface_test.go
@@ -28,65 +28,43 @@ func TestSecondsInRadians(t *testing.T) {
 	}
 }
 
-// func TestSecondHandPoint(t *testing.T) {
-// 	cases := []struct {
-// 		time  time.Time
-// 		point Point
-// 	}{
-// 		{simpleTime(0, 0, 30), Point{0, -1}},
-// 		{simpleTime(0, 0, 45), Point{-1, 0}},
-// 	}
-
-// 	for _, c := range cases {
-// 		t.Run(testName(c.time), func(t *testing.T) {
-// 			got := secondHandPoint(c.time)
-// 			if !roughlyEqualPoint(got, c.point) {
-// 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
-// 			}
-// 		})
-// 	}
-// }
-
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-// 	want := Point{X: 150, Y: 150 + 90}
-// 	got := SecondHand(tm)
-
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v", got, want)
-// 	}
-// }
-
-func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-	b := bytes.Buffer{}
-
-	SVGWriter(&b, tm)
-
-	svg := SVG{}
-
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	want := Line{150, 150, 150, 60}
-
-	for _, line := range svg.Line {
-		if line == want {
-			return
-		}
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
 	}
 
-	t.Errorf("Expected to find the second hand with of %+v, in the SVG output %+v", want, svg.Line)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
 }
 
-func roughlyEqualFloat64(a, b float64) bool {
-	const equalityThreshold = 1e-7
-	return math.Abs(a-b) < equalityThreshold
-}
-
-func roughlyEqualPoint(a, b Point) bool {
-	return roughlyEqualFloat64(a.X, b.X) &&
-		roughlyEqualFloat64(a.Y, b.Y)
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if line == l {
+			return true
+		}
+	}
+	return false
 }
 
 func simpleTime(hours, minutes, seconds int) time.Time {
