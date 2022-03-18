8acb11425425ec91e809ea2fe30f06dc1f76db92
Tom Koch <tomkoch@mailbox.org>
2022-03-18 09:56:57 +0100

    Refactor clockface

--- 16-maths/clockface.go
+++ 16-maths/clockface.go
@@ -1,9 +1,37 @@
 package clockface
 
 import (
+	"encoding/xml"
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
+
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
+}
 
 // A Point represents a two dimensional Cartesian coordinate
 type Point struct {
@@ -11,19 +39,40 @@ type Point struct {
 	Y float64
 }
 
-const secondHandLength = 90
-const clockCentreX = 150
-const clockCentreY = 150
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
+)
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
-func SecondHand(t time.Time) Point {
+func secondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
 	p = Point{p.X, -p.Y}
 	p = Point{p.X + clockCentreX, p.Y + clockCentreY} //translate
-	return p
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%f" y2="%f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
+
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
 
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
--- 16-maths/clockface_test.go
+++ 16-maths/clockface_test.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -26,34 +28,55 @@ func TestSecondsInRadians(t *testing.T) {
 	}
 }
 
-func TestSecondHandPoint(t *testing.T) {
-	cases := []struct {
-		time  time.Time
-		point Point
-	}{
-		{simpleTime(0, 0, 30), Point{0, -1}},
-		{simpleTime(0, 0, 45), Point{-1, 0}},
+// func TestSecondHandPoint(t *testing.T) {
+// 	cases := []struct {
+// 		time  time.Time
+// 		point Point
+// 	}{
+// 		{simpleTime(0, 0, 30), Point{0, -1}},
+// 		{simpleTime(0, 0, 45), Point{-1, 0}},
+// 	}
+
+// 	for _, c := range cases {
+// 		t.Run(testName(c.time), func(t *testing.T) {
+// 			got := secondHandPoint(c.time)
+// 			if !roughlyEqualPoint(got, c.point) {
+// 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+// 			}
+// 		})
+// 	}
+// }
+
+// func TestSecondHandAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+// 	want := Point{X: 150, Y: 150 + 90}
+// 	got := SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+// }
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+	SVGWriter(&b, tm)
+
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150.000000"
+	y2 := "60.000000"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
 	}
 
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			got := secondHandPoint(c.time)
-			if !roughlyEqualPoint(got, c.point) {
-				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
-			}
-		})
-	}
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 + 90}
-	got := SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
 }
 
 func roughlyEqualFloat64(a, b float64) bool {
