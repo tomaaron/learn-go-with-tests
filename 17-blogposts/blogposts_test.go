6236e3eb926913c0df5f40af1ad4204f8de6a878
Tom Koch <tomkoch@mailbox.org>
2022-03-21 21:19:47 +0100

    Refactor and add description

--- 17-blogposts/blogposts_test.go
+++ 17-blogposts/blogposts_test.go
@@ -18,19 +18,24 @@ func (s StubFailingFS) Open(name string) (fs.File, error) {
 }
 
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
 
-	got := posts[0]
-	want := blogposts.Post{Title: "Post 1"}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %+v, want %+v", got, want)
-	}
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+	})
 
 	if err != nil {
 		t.Fatal(err)
@@ -46,5 +51,12 @@ func TestFailedFS(t *testing.T) {
 
 	if err == nil {
 		t.Errorf("corrupt filesystem didn't fail")
+	}
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
