9200029d85f0bc8e657be5ad3410b0c635dc1072
Tom Koch <tomkoch@mailbox.org>
2022-03-28 15:03:44 +0200

    Add benchmark

--- 18-blogrenderer/blogrenderer.go
+++ 18-blogrenderer/blogrenderer.go
@@ -29,3 +29,25 @@ func Render(w io.Writer, p Post) error {
 
 	return nil
 }
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
